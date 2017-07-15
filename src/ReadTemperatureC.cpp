10d7b1554314070dc9230a60b8c0ce9428d7c568
Rudsom de Oliveira Lima <rudsom.lima@15sprfrn.dprf.gov.br>
2017-07-15 16:30:17 -0300

    Temperatura LM92, WifiManeger e Thingspeak

---
+++ src/ReadTemperatureC.cpp
@@ -0,0 +1,53 @@
+/**
+ * LM92 Library
+ * Kerry D. Wong
+ * http://www.kerrywong.com
+ * 1/2014
+ *
+ * Read temperature in Celsius.
+ */
+
+#include <LM92.h>
+#include <Wire.h>
+#include <ThingSpeak.h>
+
+#include <ESP8266WiFi.h>
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>        //https://github.com/tzapu/WiFiManager
+
+// const char* ssid     = "papaya";
+// const char* password = "papaya2014";
+// int status = WL_IDLE_STATUS;
+WiFiClient  client;
+
+
+LM92 lm92(0,1);  //address 1, 0
+
+unsigned long myChannelNumber = 297575;
+const char * myWriteAPIKey = "95P28PYJT1I8PKUY";
+
+void setup()
+{
+      // We start by connecting to a WiFi network
+    Serial.begin(9600);
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("AutoConnectAP");
+    Serial.println("connected...yeey :)");
+    Wire.begin();
+
+    lm92.enableFaultQueue(true); //optional, make readings more tolerent to inteference
+    lm92.ResultInCelsius = true;  //change to Celsius mode
+
+    ThingSpeak.begin(client);
+
+}
+
+void loop()
+{
+    float temperatura = lm92.readTemperature();
+    Serial.println(temperatura);
+    ThingSpeak.writeField(myChannelNumber, 1, temperatura, myWriteAPIKey);
+    delay(20000);
+}
